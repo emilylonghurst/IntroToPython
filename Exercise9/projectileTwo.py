eeb580c8bc35d2a588f9c8335f0228fce4c158bb
Emily <emily_longhurst@outlook.com>
2023-01-27 16:51:02 +0000

    fixed projectiles two

--- Exercise9/projectileTwo.py
+++ Exercise9/projectileTwo.py
@@ -6,7 +6,7 @@ theta = 80 * (math.pi/180)
 g = 9.81
 v = 44
 
-print(y + (x*math.tan(theta)) - ((g*(x**2)) / (2*(v*(math.cos(theta))**2))))
+print(y + (x*math.tan(theta)) - (g*(x**2))/(2*(v*math.cos(theta))**2))
 
 x = float(input("Please enter a value: "))
 y = float(input("Please enter a value: "))
