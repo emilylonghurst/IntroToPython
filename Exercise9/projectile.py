764c1a9834b4f6cdd13aaa537fcef947abdf8854
Emily <emily_longhurst@outlook.com>
2023-01-27 16:10:07 +0000

    Added calculator function to projectile.py

--- Exercise9/projectile.py
+++ Exercise9/projectile.py
@@ -8,3 +8,8 @@ g = 9.81
 v = float(input("Please input the initial velocity (m/s): "))
 
 print(y + (x*(math.tan(thetaNew))) - ((g*(x**2)) / (2*(v*((math.cos(thetaNew))**2)))))
+
+x = float(input("Please enter a value: "))
+y = float(input("Please enter a value: "))
+
+print(x+y)
