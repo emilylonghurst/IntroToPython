263d163f29ae0724cc1772e38cfdf480299df100
Emily <emily_longhurst@outlook.com>
2023-02-02 22:30:44 +0000

    - Changed order of prompts so that the operation is asked for before the second number - Added loop so that the program runs until the user types 'q' when prompted for the first number - Fixed second-chance inputs being saved as the first incorrect variable type (i.e. string) instead of floats - Added the option to write symbols for the chosen operation - Added a loop to check if the operation chosen is viable from a new list of possible operations: operation_list

--- Exercise10/calculator.py
+++ Exercise10/calculator.py
@@ -1,40 +1,61 @@
-import math
+
 
 print("This program will calculate an operation of your choosing when given two numbers.")
 
-first_number = input("Please enter your first number: ")
+# while loop keeps the program running unless the user enters q when prompted for first_number
 
-# Checks if first_number is a number (.isdigit() does not work for floats)
-try:
-    first_number = float(first_number)
-    
-except ValueError:
-    first_number = input("Please enter your first number: ")
+while True:
+    first_number = input("Please enter your first number (or type q to quit): ")
 
-second_number = input("Please enter your second number: ")
+    if first_number == "q":
+        break
 
-try:
-    second_number = float(second_number)
+    # Checks if first_number is a number (.isdigit() does not work for floats)
 
-except ValueError:
-    second_number = input("Please enter your second number: ")
+    try:
+        first_number = float(first_number)
 
-print("Possible operations: Add, Subtract, Multiply, Divide, Power, Root")
+    except ValueError:
+        first_number = float(input("Please enter your first number: "))
 
-operation_input = input("Please type the operation you wish to perform from the list above: ")
+# asks user for an operation
 
-try:
-    if operation_input.lower() == "add":
-        print(first_number + second_number)
-    elif operation_input.lower() == "subtract":
-        print(first_number - second_number)
-    elif operation_input.lower() == "multiply":
-        print(first_number * second_number)
-    elif operation_input.lower() == "divide":
-        print(first_number / second_number)
-    elif operation_input.lower() == "power":
-        print(first_number ** second_number)
-    elif operation_input.lower() == "root":
-        print(first_number ** (1 / second_number))
-except ValueError:
+    print("Please select an operator: Add, Subtract, Multiply, Divide, Power, Root")
+
     operation_input = input("Please type the operation you wish to perform from the list above: ")
+
+    operation_list = ["add", "+", "subtract", "-", "multiply", "*", "divide", "/", "root"]
+
+# while loop checks if the operation is within operation_list (permitted operations)
+    while True:
+        if operation_input.lower() in operation_list:
+
+            # asks user for another number and checks that it is a float
+
+            second_number = input("Please enter your second number: ")
+
+            try:
+                second_number = float(second_number)
+
+            except ValueError:
+                second_number = float(input("Please enter your second number: "))
+
+            # if statements perform the selected operation regardless of string, symbol or capitalisation
+
+            if operation_input.lower() == "add" or operation_input.lower() == "+":
+                print(first_number + second_number)
+            elif operation_input.lower() == "subtract" or operation_input.lower() == "-":
+                print(first_number - second_number)
+            elif operation_input.lower() == "multiply" or operation_input.lower() == "*":
+                print(first_number * second_number)
+            elif operation_input.lower() == "divide" or operation_input.lower() == "/":
+                print(first_number / second_number)
+            elif operation_input.lower() == "power" or operation_input.lower() == "**":
+                print(first_number ** second_number)
+            elif operation_input.lower() == "root":
+                print(first_number ** (1 / second_number))
+
+            break
+
+        else:
+            operation_input = input("Please type the operation you wish to perform from the list above: ")
