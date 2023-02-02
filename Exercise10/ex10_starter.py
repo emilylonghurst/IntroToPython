2c138c2b6289a2a0bfca2f4b207ec0d104f8dfdc
Emily <emily_longhurst@outlook.com>
2023-02-02 20:28:27 +0000

    Added steps to see the logic more clearly.

--- Exercise10/ex10_starter.py
+++ Exercise10/ex10_starter.py
@@ -12,22 +12,22 @@ pattern = os.path.join(hdir,'*')
 # TODO: Use the glob.glob() function to obtain the list of filenames
 
 file_list = glob.glob(pattern)
-print(file_list)
-# print(pattern)
+# print(file_list)
 
 # TODO: use os.path.getsize to find each file's size
 
-# file_size = os.path.getsize(files)
-
-# for e in files:
-#     print(os.path.getsize(e))
+# for file in file_list:
+#     print(os.path.getsize(file))
 
 # TODO: Add a test to only display files that are not zero length
+
+# for file in file_list:
+#     if (os.path.getsize(file)) != 0:
+#         print(os.path.getsize(file))
+
+# TODO: Remove the leading directory name(s) from each filename before you print it - 
+# using os.path.basename()
 
 for file in file_list:
     if (os.path.getsize(file)) != 0:
         print("Basename: " + os.path.basename(file) + "  Size: " + str(os.path.getsize(file)) + " bytes")
-
-
-# TODO: Remove the leading directory name(s) from each filename before you print it - 
-# using os.path.basename()
