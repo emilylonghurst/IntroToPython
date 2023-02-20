60ae8fe2841ef04048ae1ae41e8bff8db094dfb5
Emily <emily_longhurst@outlook.com>
2023-02-20 18:50:27 +0000

    Changed to set

--- Exercise12/lotto.py
+++ Exercise12/lotto.py
@@ -1,18 +1,17 @@
 
 import random
 
-my_tup = ()
-i = 0
+my_set = set()
 
-while i < 6:
+while len(my_set) < 6:
     num = random.randint(1, 10)
-    my_tup += num,
-    i += 1
+    my_set.add(num)
 
 print('Your lotto numbers are: ')
+
 # # Print the numbers in tuple format
 # print(my_tup)
 
 # Print just the numbers with spaces
-for element in my_tup:
+for element in my_set:
     print(element, end=' ')
