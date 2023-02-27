931b7b2450a202de537b8bfc094b90f5ab5c6357
Emily <emily_longhurst@outlook.com>
2023-02-27 00:22:15 +0000

    Includes history when pressing h and has some error checking.

--- calculatorList&Functions.py
+++ calculatorList&Functions.py
@@ -29,14 +29,11 @@ def check_operator(operation):
         return check_operator(input("Please type the operation you wish to perform from the list above: "))
 
 
-# def check_length(userList):
-#     print(len(userList))
-#     if len(userList) != 3:
-#       return check_length(input("Please enter your equation in the format num operator num (or type q to quit): ").split())
-#     else:
-#         return userList
-
 def check_length(userList):
+
+    if userList == "q" or userList == "Q":
+        return userList
+
     if userList == " ":
         return check_length(input("Please enter your equation in the format num operator num (or type q to quit): "))
     else:
@@ -51,16 +48,21 @@ def check_length(userList):
 print("This program will calculate an operation of your choosing when given two numbers.")
 
 user_list = [0]
+history = []
 
 while user_list[0] != "q":
     user_input = (input("Please enter your equation in the format num operator num (or type q to quit): "))
 
     if user_input[0] == "q" or user_input[0] == "Q":
         break
+    elif user_input[0] == "h" or user_input[0] == "H":
+        for x in history:
+            print(x)
 
-    # if len(user_list) < 3:
-    #    repeat again pls
     user_list = check_length(user_input)
+
+    if user_list == "q" or user_list == "Q":
+        break
 
     first_number = user_list[0]
     first_number = check_number(first_number)
@@ -70,5 +72,7 @@ while user_list[0] != "q":
 
     second_number = user_list[2]
     second_number = check_number(second_number)
+
+    history.append(user_input)
 
     calc()
